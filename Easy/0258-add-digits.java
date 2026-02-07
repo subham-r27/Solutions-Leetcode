9aaccdfc8c2b0c7322d9d424526deb634a6bad84
SUBHAM ROUT <160852219+subham-r27@users.noreply.github.com>
2026-02-07 22:03:18 +0530

    Create 0258-add-digits.java

---
+++ Easy/0258-add-digits.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
+
+/*digital root
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
+    }
+}
+*/
+
+
