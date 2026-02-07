1cec30033ba4350999fb0d1f5b3a7d62b4074c93
SUBHAM ROUT <160852219+subham-r27@users.noreply.github.com>
2026-02-08 02:36:34 +0530

    Update 0229-majority-element-ii.java

--- Medium/0229-majority-element-ii.java
+++ Medium/0229-majority-element-ii.java
@@ -1,3 +1,40 @@
+//Using Boyer Moore Algorithm
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> list=new ArrayList<>();
+        int el1=0,el2=0,c1=0,c2=0;
+        for (int num : nums) {
+            if (num == el1) {
+                c1++;
+            } else if (num == el2) {
+                c2++;
+            } else if (c1 == 0) {
+                el1 = num;
+                c1 = 1;
+            } else if (c2 == 0) {
+                el2 = num;
+                c2 = 1;
+            } else {
+                c1--;
+                c2--;
+            }
+        }
+        c1=0;c2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==el1){
+                c1++;
+            }
+            else if(nums[i]==el2){
+                c2++;
+            }
+        }
+        if(c1>(nums.length/3)) list.add(el1);
+        if(c2>(nums.length/3)) list.add(el2);
+        return list;
+    }
+}
+
+
 /* Using HASHMAP
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
