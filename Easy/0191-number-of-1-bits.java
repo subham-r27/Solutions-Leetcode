a9b62f48ae203ba728b765b4dc003cbca29ff8b8
SUBHAM ROUT <160852219+subham-r27@users.noreply.github.com>
2026-02-08 19:54:54 +0530

    Create 0191-number-of-1-bits.java

---
+++ Easy/0191-number-of-1-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int c=0;
+        while(n!=0){
+            if(n%2==1) c+=1;
+            n=n/2;
+        }
+        return c;
+    }
+}
