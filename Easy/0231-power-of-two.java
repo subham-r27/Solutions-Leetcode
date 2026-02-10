47b71ceebdd59dee1f88ffc08a2a9149ea4edbca
SUBHAM ROUT <160852219+subham-r27@users.noreply.github.com>
2026-02-10 23:56:21 +0530

    Create 0231-power-of-two.java

---
+++ Easy/0231-power-of-two.java
@@ -0,0 +1,34 @@
+//Optimal
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        else if((n & (~(n-1))) == n) return true;
+        return false;
+    }
+}
+// EXPLANATION BELOW ----------------------------------------------------------------
+/* 
+8 - 001000
+7 - 000111
+~7 - 111000
+ so, 8 & ~7 - 001000 which is 8
+ so, if n & ~(n-1)  is equal to n then it is power of n
+*/
+
+//Brute
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        String s="";
+        while(n!=0){
+            if(n%2==1) s+="1";
+            else s+="0";
+            n=n/2;
+        }
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1') count++;
+        }
+        if(count==1) return true;
+        return false;
+    }
+}
