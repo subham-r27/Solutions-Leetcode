3f1f24431d82f0018858584893aa134e08d752a7
SUBHAM ROUT <160852219+subham-r27@users.noreply.github.com>
2026-02-16 20:27:07 +0530

    Update 0050-powx-n.java

--- Medium/0050-powx-n.java
+++ Medium/0050-powx-n.java
@@ -4,3 +4,17 @@ class Solution {
     }
 }
 
+class Solution {
+    public double myPow(double x, int n) {
+        long p =n;
+        if( n < 0) return 1/power(x,-p);
+        return power(x,p);
+    }
+    public double power( double x, long n){
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if( n % 2 == 0) return power(x*x,n/2);
+        return x*power(x,n-1);
+    }
+}
+
