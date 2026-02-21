98227b6696cbc9ea76879b1e30e30695a6c6d438
SUBHAM ROUT <160852219+subham-r27@users.noreply.github.com>
2026-02-21 23:27:14 +0530

    Create 0796-rotate-string.java

---
+++ Easy/0796-rotate-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()==goal.length()){
+            s=s+s;
+            if(s.contains(goal)) return true;
+        }
+        return false;
+    }
+}
